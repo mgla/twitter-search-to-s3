bc3c1cb8d9613b969955fd6557ba9616fe9023d5
Maik Glatki <maik@glatki.eu>
2018-04-30 12:13:44 +0000

    Add env.sh file for development environment

---
+++ env.sh
@@ -0,0 +1,9 @@
+# source this
+export SEARCH_TERM=foo
+export S3_BUCKET=foobucket
+export S3_PREFIX=foo/
+export S3_REGION=eu-west-1
+export TWITTER_CONSUMER_KEY=
+export TWITTER_CONSUMER_SECRET=
+export TWITTER_ACCESS_TOKEN=
+export TWITTER_ACCESS_TOKEN_SECRET=
